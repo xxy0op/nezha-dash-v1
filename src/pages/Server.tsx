acf0c05d6e65f25e61a938e3ab8f9ff9b2f7886f
wind001 <7559527@gmail.com>
2025-11-14 00:57:55 +0800

    fix

--- src/pages/Server.tsx
+++ src/pages/Server.tsx
@@ -373,7 +373,6 @@ export default function Servers() {
     ))}
   </section>
 )}
-      )}
 {(inline === "0" || inline === "1") && (
   <section ref={containerRef} className="grid grid-cols-1 gap-2 md:grid-cols-2 mt-6 server-card-list flex lg:hidden">
     {filteredServers.map((serverInfo) => (
