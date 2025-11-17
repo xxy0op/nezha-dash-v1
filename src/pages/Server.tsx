412c62e70dcdd5b5c944f1f3b2f3245f3bf25303
wind001 <7559527@gmail.com>
2025-11-17 16:16:59 +0800

    Update Server.tsx

--- src/pages/Server.tsx
+++ src/pages/Server.tsx
@@ -29,6 +29,7 @@ export default function Servers() {
   })
   const { lastMessage, connected } = useWebSocketContext()
   const { status } = useStatus()
+
   const [showMap, setShowMap] = useState<string>("0")
   const [inline, setInline] = useState<string>("1")
   const containerRef = useRef<HTMLDivElement>(null)
@@ -50,6 +51,205 @@ export default function Servers() {
     sessionStorage.setItem("scrollPosition", String(containerRef.current?.scrollTop || 0))
   }
 
+  useEffect(() => {
+    const checkInlineSettings = () => {
+      const isMobile = window.innerWidth < 768
+
+      if (!isMobile) {
+        const inlineState = localStorage.getItem("inline")
+        if (window.ForceCardInline) {
+          setInline("1")
+        } else if (inlineState !== null) {
+          setInline(inlineState)
+        }
+      }
+    }
+
+    checkInlineSettings()
+
+    window.addEventListener("resize", checkInlineSettings)
+
+    return () => {
+      window.removeEventListener("resize", checkInlineSettings)
+    }
+  }, [])
+
+  useEffect(() => {
+    const showMapState = localStorage.getItem("showMap")
+    if (window.ForceShowMap) {
+      setShowMap("1")
+    } else if (showMapState !== null) {
+      setShowMap(showMapState)
+    }
+  }, [])
+
+  useEffect(() => {
+    const savedGroup = sessionStorage.getItem("selectedGroup") || "All"
+    setCurrentGroup(savedGroup)
+
+    restoreScrollPosition()
+  }, [])
+
+  const nezhaWsData = lastMessage ? (JSON.parse(lastMessage.data) as NezhaWebsocketResponse) : null
+
+  const groupTabs = [
+    "All",
+    ...(groupData?.data
+      ?.filter((item: ServerGroup) => {
+        return Array.isArray(item.servers) && item.servers.some((serverId) => nezhaWsData?.servers?.some((server) => server.id === serverId))
+      })
+      ?.map((item: ServerGroup) => item.group.name) || []),
+  ]
+
+  if (!connected && !lastMessage) {
+    return (
+      <div className="flex flex-col items-center min-h-96 justify-center ">
+        <div className="font-semibold flex items-center gap-2 text-sm">
+          <Loader visible={true} />
+          {/* {t("info.websocketConnecting")} */}
+        </div>
+      </div>
+    )
+  }
+
+  if (!nezhaWsData) {
+    return (
+      <div className="flex flex-col items-center justify-center ">
+        <p className="font-semibold text-sm">{t("info.processing")}</p>
+      </div>
+    )
+  }
+
+  let filteredServers =
+    nezhaWsData?.servers?.filter((server) => {
+      if (currentGroup === "All") return true
+      const group = groupData?.data?.find(
+        (g: ServerGroup) => g.group.name === currentGroup && Array.isArray(g.servers) && g.servers.includes(server.id),
+      )
+      return !!group
+    }) || []
+
+  const totalServers = filteredServers.length || 0
+  const onlineServers = filteredServers.filter((server) => formatNezhaInfo(nezhaWsData.now, server).online)?.length || 0
+  const offlineServers = filteredServers.filter((server) => !formatNezhaInfo(nezhaWsData.now, server).online)?.length || 0
+  const up =
+    filteredServers.reduce(
+      (total, server) => (formatNezhaInfo(nezhaWsData.now, server).online ? total + (server.state?.net_out_transfer ?? 0) : total),
+      0,
+    ) || 0
+  const down =
+    filteredServers.reduce(
+      (total, server) => (formatNezhaInfo(nezhaWsData.now, server).online ? total + (server.state?.net_in_transfer ?? 0) : total),
+      0,
+    ) || 0
+
+  const upSpeed =
+    filteredServers.reduce(
+      (total, server) => (formatNezhaInfo(nezhaWsData.now, server).online ? total + (server.state?.net_out_speed ?? 0) : total),
+      0,
+    ) || 0
+  const downSpeed =
+    filteredServers.reduce(
+      (total, server) => (formatNezhaInfo(nezhaWsData.now, server).online ? total + (server.state?.net_in_speed ?? 0) : total),
+      0,
+    ) || 0
+
+  filteredServers =
+    status === "all"
+      ? filteredServers
+      : filteredServers.filter((server) => [status].includes(formatNezhaInfo(nezhaWsData.now, server).online ? "online" : "offline"))
+
+  filteredServers = filteredServers.sort((a, b) => {
+    const serverAInfo = formatNezhaInfo(nezhaWsData.now, a)
+    const serverBInfo = formatNezhaInfo(nezhaWsData.now, b)
+
+    if (sortType !== "name") {
+      // 仅在非 "name" 排序时，先按在线状态排序
+      if (!serverAInfo.online && serverBInfo.online) return 1
+      if (serverAInfo.online && !serverBInfo.online) return -1
+      if (!serverAInfo.online && !serverBInfo.online) {
+        // 如果两者都离线，可以继续按照其他条件排序，或者保持原序
+        // 这里选择保持原序
+        return 0
+      }
+    }
+
+    let comparison = 0
+
+    switch (sortType) {
+      case "name":
+        comparison = a.name.localeCompare(b.name)
+        break
+      case "uptime":
+        comparison = (a.state?.uptime ?? 0) - (b.state?.uptime ?? 0)
+        break
+      case "system":
+        comparison = a.host.platform.localeCompare(b.host.platform)
+        break
+      case "cpu":
+        comparison = (a.state?.cpu ?? 0) - (b.state?.cpu ?? 0)
+        break
+      case "mem":
+        comparison = (formatNezhaInfo(nezhaWsData.now, a).mem ?? 0) - (formatNezhaInfo(nezhaWsData.now, b).mem ?? 0)
+        break
+      case "disk":
+        comparison = (formatNezhaInfo(nezhaWsData.now, a).disk ?? 0) - (formatNezhaInfo(nezhaWsData.now, b).disk ?? 0)
+        break
+      case "up":
+        comparison = (a.state?.net_out_speed ?? 0) - (b.state?.net_out_speed ?? 0)
+        break
+      case "down":
+        comparison = (a.state?.net_in_speed ?? 0) - (b.state?.net_in_speed ?? 0)
+        break
+      case "up total":
+        comparison = (a.state?.net_out_transfer ?? 0) - (b.state?.net_out_transfer ?? 0)
+        break
+      case "down total":
+        comparison = (a.state?.net_in_transfer ?? 0) - (b.state?.net_in_transfer ?? 0)
+        break
+      default:
+        comparison = (a.display_index ?? 0) - (b.display_index ?? 0)
+    }
+
+    return sortOrder === "asc" ? comparison : -comparison
+  })
+
+  return (
+    <div className="mx-auto w-full max-w-5xl px-0">
+      <ServerOverview
+        total={totalServers}
+        online={onlineServers}
+        offline={offlineServers}
+        up={up}
+        down={down}
+        upSpeed={upSpeed}
+        downSpeed={downSpeed}
+      />
+      <div className="flex mt-6 items-center justify-between gap-2 server-overview-controls">
+        <section className="flex items-center gap-2 w-full overflow-hidden">
+          <button
+            onClick={() => {
+              setShowMap(showMap === "0" ? "1" : "0")
+              localStorage.setItem("showMap", showMap === "0" ? "1" : "0")
+            }}
+            className={cn(
+              "rounded-[50px] bg-white dark:bg-stone-800 cursor-pointer p-[10px] transition-all border dark:border-none border-stone-200 dark:border-stone-700 hover:bg-stone-100 dark:hover:bg-stone-700 shadow-[inset_0_1px_0_rgba(255,255,255,0.2)]",
+              {
+                "shadow-[inset_0_1px_0_rgba(0,0,0,0.2)] !bg-blue-600 hover:!bg-blue-600 border-blue-600 dark:border-blue-600": showMap === "1",
+                "text-white": showMap === "1",
+              },
+              {
+                "bg-opacity-70 dark:bg-opacity-70": customBackgroundImage,
+              },
+            )}
+          >
+            <MapIcon
+              className={cn("size-[13px]", {
+                "text-white": showMap === "1",
+              })}
+            />
+          </button>
+          
           <button
             onClick={() => {
               setInline(inline === "0" ? "1" : "0")
@@ -134,20 +334,21 @@ export default function Servers() {
         </Popover>
       </div>
       {showMap === "1" && <GlobalMap now={nezhaWsData.now} serverList={nezhaWsData?.servers || []} />}
-{inline === "1" && (
-  <section ref={containerRef} className="flex flex-col gap-2 mt-6 server-inline-list overflow-x-auto lg:overflow-x-scroll lg:scrollbar-hidden hidden lg:flex">
-    {filteredServers.map((serverInfo) => (
-      <ServerCardInline now={nezhaWsData.now} key={serverInfo.id} serverInfo={serverInfo} />
-    ))}
-  </section>
-)}
-{(inline === "0" || inline === "1") && (
-  <section ref={containerRef} className="grid grid-cols-1 gap-2 md:grid-cols-2 mt-6 server-card-list flex lg:hidden">
-    {filteredServers.map((serverInfo) => (
-      <ServerCard now={nezhaWsData.now} key={serverInfo.id} serverInfo={serverInfo} />
-    ))}
-  </section>
-)}
+      
+      {inline === "1" && (
+        <section ref={containerRef} className="flex flex-col gap-2 mt-6 server-inline-list overflow-x-auto lg:overflow-x-scroll lg:scrollbar-hidden hidden lg:flex">
+          {filteredServers.map((serverInfo) => (
+            <ServerCardInline now={nezhaWsData.now} key={serverInfo.id} serverInfo={serverInfo} />
+          ))}
+        </section>
+      )}
+      {(inline === "0" || inline === "1") && (
+        <section ref={containerRef} className="grid grid-cols-1 gap-2 md:grid-cols-2 mt-6 server-card-list flex lg:hidden">
+          {filteredServers.map((serverInfo) => (
+            <ServerCard now={nezhaWsData.now} key={serverInfo.id} serverInfo={serverInfo} />
+          ))}
+        </section>
+      )}
     </div>
   )
 }
